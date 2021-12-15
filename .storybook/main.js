dbe5747aa5ad76d70d600d99a736bdab102e0c2b
Nate Radebaugh <nradebaugh@bdo.com>
2021-12-15 13:09:46 -0600

    Update storybook to latest

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,12 @@ const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  staticDirs: ["../public"],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
