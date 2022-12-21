697368a151a79f8f2247c8142bc62baba28fb4ac
Nate Radebaugh <nradebaugh@bdo.com>
2022-12-20 20:39:46 -0600

    Update to storybook&7.0.0-beta.13

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,12 @@
-const path = require("path");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "storybook-addon-next-router",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   staticDirs: ["../public"],
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack5",
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
   },
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../"),
-    });
-
-    // Return the altered config
-    return config;
-  },
+  docs: {
+    docsPage: "automatic"
+  }
 };
